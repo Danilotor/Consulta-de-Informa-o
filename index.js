ed40cb9f7c06a452d433645bfceb6a54d2528d31
Danilo-Lopes-Stabile <133702509+Danilotor@users.noreply.github.com>
2023-10-16 16:32:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let mundialDoPalmeiras = confirm("Se o Palmeiras tem Mundial clique em OK")
+
+
+if (mundialDoPalmeiras ==true){
+    alert("Palmeiras nao tem Mundial ")
+    alert("Procurando no banco de dados")
+    alert("nao encontrado, procurando novamente")
+    alert("nao encontrado ! ")
+    alert("error 401 not found ")
+    alert("error 401 not found ")
+    alert("error 401 not found ")
+    alert("error 401 not found ")
+    alert("error 401 not found ")
+} else {
+    alert("voce é sabio")
+}
+
